c68bb628fcd31576c303af87b462b9cb5b010e21
les5185 <70672721+les5185@users.noreply.github.com>
2022-10-03 01:22:06 -0500

    Add recipe create and save to disk

--- Recipe-Book/src/Main.java
+++ Recipe-Book/src/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 // class for commandline user interaction
 //Recipe(String name, String description, ArrayList<String> ingredients, ArrayList<String> instructions)
@@ -15,99 +17,93 @@ public class Main {
 	public static Recipe initalizeRecipe1() {
 
 		// from https://www.allrecipes.com/recipe/24052/brigadeiro/
-		 ArrayList<String> ingredients = new ArrayList<String>(){
-			 {
-				 add("1 (14 ounce) can sweetened condensed milk");
-				 add("3 tablespoons unsweetened cocoa");
-				 add("1 tablespoon butter");
-			 }
-			 
-		 };
-		 ArrayList<String> instructions = new ArrayList<String>(){
-			 {
-				 add("Combine condensed milk, cocoa, and butter in a medium saucepan over medium heat. Cook and stir until thickened, about 10 minutes. Remove from heat and let rest until mixture is cool enough to handle.");
-				 add("Use your hands to shape into small balls and place on a serving plate. Can be eaten at once or chilled until serving.");
-				 add("ENJOY!");
-			 }
-			 
-		 };
-		 
-		 return new Recipe("Brigadeiros", "Yummy and simple Brazilian dessert.", ingredients, instructions); 
+		ArrayList<String> ingredients = new ArrayList<String>(){
+			{
+				add("1 (14 ounce) can sweetened condensed milk");
+				add("3 tablespoons unsweetened cocoa");
+				add("1 tablespoon butter");
+			}
+			
+		};
+		ArrayList<String> instructions = new ArrayList<String>(){
+			{
+				add("Combine condensed milk, cocoa, and butter in a medium saucepan over medium heat. Cook and stir until thickened, about 10 minutes. Remove from heat and let rest until mixture is cool enough to handle.");
+				add("Use your hands to shape into small balls and place on a serving plate. Can be eaten at once or chilled until serving.");
+				add("ENJOY!");
+			}
+		};
+		
+		return new Recipe("Brigadeiros", "Yummy and simple Brazilian dessert.", ingredients, instructions); 
 	}
 	
 	public static Recipe initializeRecipe2() {
 		// from https://www.allrecipes.com/recipe/157642/homemade-flour-tortillas/
 		
 		ArrayList<String> ingredients = new ArrayList<String>(){
-			 {
-				 add("4 cups all-purpose flour");
-				 add("1 teaspoon salt");
-				 add("2 teaspoons baking powder");
-				 add("2 tablespoons lard");
-				 add("1.5 cups water"); 
-			 }
-			 
-		 };
-		 ArrayList<String> instructions = new ArrayList<String>(){
-			 {
-				 add("Whisk the flour, salt, and baking powder together in a mixing bowl. "
+			{
+				add("4 cups all-purpose flour");
+				add("1 teaspoon salt");
+				add("2 teaspoons baking powder");
+				add("2 tablespoons lard");
+				add("1.5 cups water"); 
+			}
+		};
+		ArrayList<String> instructions = new ArrayList<String>(){
+			{
+				add("Whisk the flour, salt, and baking powder together in a mixing bowl. "
 				 		+ "Mix in the lard with your fingers until the flour resembles cornmeal. "
 				 		+ "Add the water and mix until the dough comes together; "
 				 		+ "place on a lightly floured surface and knead a few minutes until smooth and elastic. D"
 				 		+ "ivide the dough into 24 equal pieces and roll each piece into a ball.");
-				 add("Preheat a large skillet over medium-high heat. "
+				add("Preheat a large skillet over medium-high heat. "
 				 		+ "Use a well-floured rolling pin to roll a dough ball into a thin, round tortilla. P"
 				 		+ "lace into the hot skillet, and cook until bubbly and golden; flip and continue cooking until golden on the other side. "
 				 		+ "Place the cooked tortilla in a tortilla warmer; continue rolling and cooking the remaining dough.");
-			 }
-			 
-		 };
-		 
-		 return new Recipe("Homemade Flour Tortillas", "Traditional flour tortillas - homemade and much better than store bought. Do not substitute vegetable oil or shortening for the lard.", ingredients, instructions); 
-		 
+			}
+		};
 		
+		return new Recipe("Homemade Flour Tortillas", "Traditional flour tortillas - homemade and much better than store bought. Do not substitute vegetable oil or shortening for the lard.", ingredients, instructions); 
 	}
 	
 	public static Recipe initializeRecipe3() {
 		// from https://www.allrecipes.com/recipe/46982/pesto-pasta-with-chicken/
 		ArrayList<String> ingredients = new ArrayList<String>(){
-			 {
-				 add("1 (16 ounce) package bow tie pasta");
-				 add("1 teaspoon olive oil");
-				 add("2 cloves garlic, minced");
-				 add("2 skinless, boneless chicken breasts, cut into bite-sized pieces");
-				 add("1 pinch crushed red pepper flakes, or to taste");
-				 add("1/2 cup pesto sauce");
-				 add("⅓ cup oil-packed sun-dried tomatoes, drained and cut into strips");
-			 }
-			 
-		 };
-		 
-		 ArrayList<String> instructions = new ArrayList<String>(){
-			 {
-				 add("Bring a large pot of lightly salted water to a boil. Add pasta and cook until al dente, 8 to 10 minutes; drain.");
-				 add("Heat oil in a large skillet over medium heat. Sauté garlic until tender, then stir in chicken and season with red pepper flakes. Cook until chicken is golden and cooked through.");
-				 add("Combine pasta, chicken, pesto, and sun-dried tomatoes in a large bowl; toss to coat evenly.");
-			 }
-			 
-		 };
-		 
-		 return new Recipe("Pesto Pasta with Chicken", "Easy and delicious. Serve with crusty bread and salad for dinner.", ingredients, instructions); 
+			{
+				add("1 (16 ounce) package bow tie pasta");
+				add("1 teaspoon olive oil");
+				add("2 cloves garlic, minced");
+				add("2 skinless, boneless chicken breasts, cut into bite-sized pieces");
+				add("1 pinch crushed red pepper flakes, or to taste");
+				add("1/2 cup pesto sauce");
+				add("⅓ cup oil-packed sun-dried tomatoes, drained and cut into strips");
+			}
+		};
+		
+		ArrayList<String> instructions = new ArrayList<String>(){
+			{
+				add("Bring a large pot of lightly salted water to a boil. Add pasta and cook until al dente, 8 to 10 minutes; drain.");
+				add("Heat oil in a large skillet over medium heat. Sauté garlic until tender, then stir in chicken and season with red pepper flakes. Cook until chicken is golden and cooked through.");
+				add("Combine pasta, chicken, pesto, and sun-dried tomatoes in a large bowl; toss to coat evenly.");
+			}
+			
+		};
+		
+		return new Recipe("Pesto Pasta with Chicken", "Easy and delicious. Serve with crusty bread and salad for dinner.", ingredients, instructions); 
 		
 		
 	}
 	
 	public static RecipeBook initializeRecipeBook() {
 		RecipeBook rb = new RecipeBook() {
-			 {
-				 add(initalizeRecipe1());
-				 add(initializeRecipe2());
-				 add(initializeRecipe3());
-			 }
-			 
-		 };
-		 
-		 return rb;
+			{
+				add(initalizeRecipe1());
+				add(initializeRecipe2());
+				add(initializeRecipe3());
+			}
+			
+		};
+		
+		return rb;
 	}
 	
 	
@@ -116,7 +112,65 @@ public class Main {
 		
 		RecipeBook mainBook = initializeRecipeBook();
 		System.out.println(mainBook);
-		
+		int i=0;
+        Scanner myObj = new Scanner(System.in);  
+        while (i==0) {
+            ArrayList<String> ingredients = new ArrayList<>();
+            ArrayList<String> instructions = new ArrayList<>();
+
+            System.out.println("Create a recipe");
+            System.out.println("Recipe Name: ");
+            String recipeName = myObj.nextLine();
+            System.out.println("Recipe Description: ");
+            String recipeDescription = myObj.nextLine();
+
+            System.out.println("Recipe Ingredients: ");
+            int j=0;
+            int count=1;
+            while(j==0){
+                System.out.println("Please enter Ingredient "+ count);
+                String ing = myObj.nextLine();
+                ingredients.add(ing);
+                System.out.println("Enter more ingredients? Y/N");
+                String choice = myObj.next();
+                if (choice == "Y"){
+                    continue;
+                }
+                else j=1;
+            }
+
+            System.out.println("Recipe Instructions: ");
+            int k=0;
+            int count_instructions=1;
+            while(k==0){
+                System.out.println("Please enter Instructions "+ count_instructions);
+                String ins = myObj.nextLine();
+                instructions.add(ins);
+                System.out.println("Enter more instructions? Y/N");
+                String choice = myObj.next();
+                if (choice == "Y"){
+                    continue;
+                }
+                else k=1;
+            }
+
+            RecipeBook recipebook = new RecipeBook(){ {
+				//this line keeps giving me an error, can you guys help me out with it?
+                addRecipe(recipeName, recipeDescription, ingredients, instructions);
+            }
+            };
+
+            //if above line x works, use this
+            // RecipeBook recipeBook2 = new RecipeBook();
+            // recipeBook2.addRecipe(recipeName, recipeDescription, ingredients, instructions);
+
+            System.out.println("Do you want to add another recipe? Y/N");
+            String choice = myObj.next();
+            if (choice=="Y"){
+                continue;
+            }
+            else i=1;
+        }
 		
 
 	}
--- Recipe-Book/src/RecipeBook.java
+++ Recipe-Book/src/RecipeBook.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.io.*;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.APPEND;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class RecipeBook extends ArrayList<Recipe> implements Serializable {
 	
@@ -10,6 +15,14 @@ public class RecipeBook extends ArrayList<Recipe> implements Serializable {
 		this.add(r); 
 	}
 	
-	
-	
+	public void saveToDisk(Recipe r){
+        try{
+            Path currentRelativePath = Paths.get("");
+            String s = currentRelativePath.toAbsolutePath().toString();
+            Files.write(Paths.get(s+"/Data.txt"),(""+r).getBytes(), APPEND);
+
+        } catch(IOException e){
+            System.out.println(toString());
+        }
+    }
 }
